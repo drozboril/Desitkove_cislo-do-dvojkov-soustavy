f9cc0b0924186946f24f8986c7ec8a171ef6cc81
drozboril <rozborildavid77@gmail.com>
2022-11-24 19:16:19 +0100

    Uživatel zadá číslo od 0-15, program vypíše binární hodnotu vstupu

---
+++ Desítkové číslo do dvojkové soustavy/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binární_čísla
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zadejte číslo od 0 do 15");
+            int dec = int.Parse(Console.ReadLine());
+            if (dec < 0 || dec > 15)
+            {
+                Console.WriteLine("Chyba zadání");
+                Console.ReadLine();
+            }
+            else
+            {
+                string bin = Convert.ToString(dec, 2);
+                Console.WriteLine("Zadané čísla {0} je ve dvojkové soustavě {1}", dec, bin);
+                Console.ReadLine();
+            }
+        }
+    }
+}   
